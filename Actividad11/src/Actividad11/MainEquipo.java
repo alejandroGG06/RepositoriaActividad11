a2317ddbf9630ab2bfe73c5fed7f4332f7638d19
juandaalean <juanalean2525@gmail.com>
2024-02-21 20:50:18 +0100

    Cambios en el main by:juanda

--- Actividad11/src/Actividad11/MainEquipo.java
+++ Actividad11/src/Actividad11/MainEquipo.java
@@ -45,6 +45,8 @@ package Actividad11;
 
 				System.out.println();
 				System.out.println("¿El equipo es igual al primer equipo creado? " + equipo.IgualEquipo(equipos[0]));
+				
+				System.out.println("No me han aceptado esta linea :(");
 			}
 
 		}
